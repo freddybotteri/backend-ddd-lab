5655ae10303cd7e0f9d89dc36a855e2e8d077ed7
FREDDY MILLA <freddybotteri@gmail.com>
2020-03-14 16:43:36 +0100

    change cqrs creator

--- src/Contexts/Lab/Auth/domain/AuthRepository.ts
+++ src/Contexts/Lab/Auth/domain/AuthRepository.ts
@@ -4,5 +4,7 @@ import { Nullable } from '../../../Shared/domain/Nullable';
 export default interface AuthRepository {
   save(auth: Auth): Promise<void>;
 
-  search(id: string): Promise<Nullable<Auth>>;
+  findAll(id: string): Promise<any>;
+
+  signin(email: string,password: string): Promise<Nullable<Auth>>;
 }
